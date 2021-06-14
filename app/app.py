819101986e478b047d12e6abbe19a41d0059458f
Eric <eric.miller@caserta.com>
2021-06-13 19:43:55 -0700

    Updated flask environment to work properly

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-env = "dev"
+env = os.environ.get("ENV")
+port = os.environ.get("PORT")
 
 @app.route("/")
 def index():
     return f"Hello World! This is the {env.lower()} environment!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
